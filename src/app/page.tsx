194e618ffde83ee9f0d71d73a76cc2f3ae3a32d1
Khaled Melgharib <khaledmelgharib@gmail.com>
2025-08-21 23:34:15 +0300

    Remove unused MainContent import and component from App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Home from './Home'; 
-import MainContent from "./Components/MainContent";
 
 const App = () => {
   return (
     <div style={{}}>
       <Home/>
-      {/* <MainContent /> */}
     </div>
   );
 };
